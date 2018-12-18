2451be530669c98ab4e9dea881bb2e779be5db49
kyung <leeseok8347@gmail.com>
2018-12-18 17:35:56 +0900

    add inheritance with Treasure

--- contracts/Treasure.sol
+++ contracts/Treasure.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.4.24;
 
+import "./ERC1155/ERC1155MixedFungibleMintable.sol";
+
 /**
  * @title Treasure
  * @dev Treasure Contract
  */
-contract Treasure {
+contract Treasure is ERC1155MixedFungibleMintable {
 
 }
