38572c026af473239cda7704ded040bfb6dec424
gijinBang <rlwlsdnlwjem2@gmail.com>
2018-12-18 17:09:15 +0900

    erc1155 added

--- contracts/Area.sol
+++ contracts/Area.sol
@@ -6,11 +6,30 @@ pragma solidity ^0.4.24;
  */
 contract Area {
     
+    uint256 AreaWeight;
+    uint256 AreaBalance;
+    uint256 depositAmount;
+
     function getCurrentBeneficiaryInfo() public view returns(address beneficiary, uint256 ratio){
         return (0x6f0f5673d69b4608ac9be5887b2f71f20d0c3587, 10**17);
     }
 
     function moveCursor() public returns (bool success){
         return true;
+    }
+
+    function buy() public payable returns (bool success){
+        // mintingAmount = calculatePurchaseReturn(totalSupply, AreaBalance, AreaWeight, depositAmount)
+        // batchMint(mintingAmount, msg.sender, "Area0Security")
+        // add holder to linked list if not exist
+        // set time lock
+        // (balance added to the contract)
+        return true;
+
+    }
+    function sell() public returns (bool success){
+        // buringAmount = calculateSaleReturn(totalSupply, AreaBalance, AreaWeight, depositAmount)
+        // batchBurn(burningAmount, msg.sender);
+        // send balance to the msg.sender
     }
 }
---
+++ contracts/ERC1155/ERC1155Mintable.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.4.24;
+
+import "./ERC1155.sol";
+
+/**
+    @dev Mintable form of ERC1155
+    Shows how easy it is to mint new items.
+*/
+contract ERC1155Mintable is ERC1155 {
+
+    // id => creators
+    mapping (uint256 => address) public creators;
+
+    // A nonce to ensure we have a unique id each time we mint.
+    uint256 public nonce;
+
+    modifier creatorOnly(uint256 _id) {
+        require(creators[_id] == msg.sender);
+        _;
+    }
+
+    // Creates a new token type and assings _initialSupply to minter
+    function create(uint256 _initialSupply, string _name, string _uri) external returns(uint256 _id) {
+
+        _id = ++nonce;
+        creators[_id] = msg.sender;
+        balances[_id][msg.sender] = _initialSupply;
+
+        // Transfer event with mint semantic
+        emit TransferSingle(msg.sender, 0x0, msg.sender, _id, _initialSupply);
+
+        if (bytes(_name).length > 0)
+            emit Name(_name, _id);
+
+        if (bytes(_uri).length > 0)
+            emit URI(_uri, _id);
+    }
+
+    // Batch mint tokens. Assign directly to _to[].
+    function mint(uint256 _id, address[] _to, uint256[] _quantities) external creatorOnly(_id) {
+
+        for (uint256 i = 0; i < _to.length; ++i) {
+
+            address to = _to[i];
+            uint256 quantity = _quantities[i];
+
+            // Grant the items to the caller
+            balances[_id][to] = quantity.add(balances[_id][to]);
+
+            // Emit the Transfer/Mint event.
+            // the 0x0 source address implies a mint
+            // It will also provide the circulating supply info.
+            emit TransferSingle(msg.sender, 0x0, to, _id, quantity);
+
+            if (to.isContract()) {
+                require(IERC1155TokenReceiver(to).onERC1155Received(msg.sender, msg.sender, _id, quantity, '') == ERC1155_RECEIVED);
+            }
+        }
+    }
+
+    function setURI(string _uri, uint256 _id) external creatorOnly(_id) {
+        emit URI(_uri, _id);
+    }
+
+    function setName(string _name, uint256 _id) external creatorOnly(_id) {
+        emit Name(_name, _id);
+    }
+}
